35036a21f3a7b29431f8e02b6a275b093b0b11d9
Ben Best <ben@ecoquants.com>
2020-05-26 04:14:33 -0700

    pdf off

--- functions.R
+++ functions.R
@@ -1,4 +1,4 @@
-city2zip <- function(city){
+city2zip <- function(city, redo = F){
   # city = "Delhi, India"
   
   # load libraries ----
@@ -13,13 +13,18 @@ city2zip <- function(city){
   
   # set variables ----
   # use API: https://nominatim.org/release-docs/develop/api/Search/
-  osm_url   <- "https://nominatim.openstreetmap.org/search.php"
-  city_s    <-  str_replace(city, "[ ,]+", "-")
-  city_url  <- glue("{osm_url}?q={city_s}&polygon_geojson=1&format=json")
-  city_json <- here(glue("data/city_{city_s}.json"))
-  city_geo  <- here(glue("data/city_{city_s}.geojson"))
-  city_shp  <- here(glue("data/city_{city_s}/city_{city_s}.shp"))
-  city_zip  <- here(glue("data/city_{city_s}.zip"))
+  osm_url    <- "https://nominatim.openstreetmap.org/search.php"
+  city_s     <-  str_replace(city, "[ ,]+", "-")
+  city_url   <- glue("{osm_url}?q={city_s}&polygon_geojson=1&format=json")
+  city_json  <- here(glue("data/city_{city_s}.json"))
+  city_geo   <- here(glue("data/city_{city_s}.geojson"))
+  city_shp   <- here(glue("data/city_{city_s}/city_{city_s}.shp"))
+  city_zip   <- here(glue("data/city_{city_s}.zip"))
+  city_paths <- list(geojson = city_geo, zip = city_zip)
+  
+  # skip if already done, unless redo = T
+  if (file.exists(as.character(city_paths)) & !redo)
+    return(city_paths)
   
   # download data using API ----
   download.file(city_url, city_json)
@@ -48,17 +53,17 @@ city2zip <- function(city){
   writeLines(f, city_geo)
   
   # add centroid lon/lat and radius in miles ----
-  # p <- read_sf(city_geo)
-  # ctr <- st_centroid(p$geometry) %>% st_coordinates()
-  # area_sqmi <- st_area(p$geometry) %>% set_units(mi^2)
-  # area_sqm  <- st_area(p$geometry) %>% set_units(m^2)
-  # p <- p %>% 
-  #   mutate(
-  #     lon  = ctr[,'X'],
-  #     lat  = ctr[,'Y'],
-  #     r_mi = sqrt(area_sqmi/pi),
-  #     r_m  = sqrt(area_sqm/pi))
-  # write_sf(p, city_geo, delete_dsn = T)
+  p <- read_sf(city_geo)
+  ctr <- st_centroid(p$geometry) %>% st_coordinates()
+  area_sqmi <- st_area(p$geometry) %>% set_units(mi^2)
+  area_sqm  <- st_area(p$geometry) %>% set_units(m^2)
+  p <- p %>%
+    mutate(
+      lon  = ctr[,'X'],
+      lat  = ctr[,'Y'],
+      r_mi = sqrt(area_sqmi/pi),
+      r_m  = sqrt(area_sqm/pi))
+  write_sf(p, city_geo, delete_dsn = T)
   
   # write shapefile package for upload to GEE ----
   p <- read_sf(city_geo)
@@ -72,7 +77,5 @@ city2zip <- function(city){
     city_json), recursive = T, force = T)
   
   # return paths to files generated
-  list(
-    geojson = city_geo,
-    zip     = city_zip)
+  city_paths
 }
